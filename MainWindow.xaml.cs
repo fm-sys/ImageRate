6a2f728f286c4e1b4f4a93a6623e1cb2740fe6a9
fm-sys <64581222+fm-sys@users.noreply.github.com>
2023-11-19 23:25:08 +0100

    New release

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,30 +1,14 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage.AccessCache;
 using Windows.Storage.Pickers;
 using Windows.Storage;
-using Microsoft.UI;
-using System.Diagnostics;
 using Microsoft.UI.Xaml.Media.Imaging;
-using ExifLibrary;
 using Windows.Storage.FileProperties;
-using Windows.UI.Core;
-using Windows.UI.ViewManagement;
-using static System.Net.Mime.MediaTypeNames;
 using System.Threading.Tasks;
-using System.Reflection;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
